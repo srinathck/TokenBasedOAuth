6cf38dc29b6622aeef0f0df98e74c44ba116dc07
srinathck <srinathck@github.com>
2018-09-21 19:31:23 -0500

    Working UI DB and token based OAuth implemented.

---
+++ TokenBasedOAuth.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace TokenBasedOAuth.Web.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using (var db = new EmployeeDBEntities())
+            {
+                return db.Employees.ToList();
+            }
+        }
+    }
+}
